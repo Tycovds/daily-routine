eaac15f32ade16453030d2c87091671a7c6dedd5
Tyco van der Steen <tycovds@gmail.com>
2022-03-31 19:34:52 +0200

    generated assets

--- vue.config.js
+++ vue.config.js
@@ -5,11 +5,11 @@ module.exports = defineConfig({
     themeColor: "#e0ddaa",
     msTileColor: '#203239',
     iconPaths: {
-      favicon32: "./img/icons/favicon.svg",
-      favicon16: "./img/icons/favicon.svg",
-      appleTouchIcon: "./img/icons/favicon.svg",
+      favicon32: "./img/icons/favicon-32x32.png",
+      favicon16: "./img/icons/favicon-16x16.png",
+      appleTouchIcon: "./img/icons/apple-touch-icon-120x120.png",
       maskIcon: "./img/icons/favicon.svg",
-      msTileImage: "./img/icons/favicon.svg"
+      msTileImage: "./img/icons/msapplication-icon-144x144.png"
     }
   }
 })
