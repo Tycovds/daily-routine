f9522818e94a5302f25b32ca16b9ea84f5acc2cd
Tyco van der Steen <tycovds@gmail.com>
2022-03-31 14:21:41 +0200

    pwa config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    themeColor: "#e0ddaa",
+    msTileColor: '#203239',
+    iconPaths: {
+      favicon32: "./img/icons/favicon.svg",
+      favicon16: "./img/icons/favicon.svg",
+      appleTouchIcon: "./img/icons/favicon.svg",
+      maskIcon: "./img/icons/favicon.svg",
+      msTileImage: "./img/icons/favicon.svg"
+    }
+  }
 })
